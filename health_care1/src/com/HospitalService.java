4b862706b11cf584d84737a9b0860e6f7a96c4ac
damitheranga <damitheb@gmail.com>
2020-03-27 10:10:21 +0530

    first

---
+++ health_care1/src/com/HospitalService.java
@@ -0,0 +1,22 @@
+package com;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Hospital;
+
+@Path("/Hospital")
+
+public class HospitalService {
+	Hospital itemObj = new Hospital();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+		return "Hello Welcome Our Hospistal";
+	}
+
+}
